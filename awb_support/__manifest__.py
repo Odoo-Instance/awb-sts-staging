0d67e6a9aeb3010e6d665f03ca267b04ac5fd7f7
Pankaj <114057234+awbpankaj@users.noreply.github.com>
2022-11-07 17:49:31 +0530

    [ADD: Added new changes for blue tick field and update records in front end from backend in ticket]

--- awb_support/__manifest__.py
+++ awb_support/__manifest__.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name': 'AWB Support Ticketing System',
-    'summary': "AWB Support Ticketing System Custom",
-    'version': '15.1',
-    'author': "Achieve Without Borders, Inc.",
-    'website': "https://www.achievewithoutborders.com/",
+    'name' : 'AWB Support',
+    'version' : '15.1',
+    'summary': 'AWB Support',
+    'sequence': 10,
     'description': """
 HELPDESK
 ====================
-AWB Support Ticketing System """,
+AWB Support """,
     'category': 'Support',
-    'depends': ['helpdesk', 'website_helpdesk_form'],
+    'depends' : ['helpdesk','website_helpdesk_form'],
     'data': [
         'views/helpdesk_ticket_view.xml',
         'views/helpdesk_templates.xml',
